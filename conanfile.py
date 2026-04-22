04c7bca881e909e5cf2a89e258bf87f710b5aa71
Ben Baker <bakerb845@gmail.com>
2026-04-22 12:26:18 -0600

    bringing in depenedency

---
+++ conanfile.py
@@ -0,0 +1,69 @@
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake, CMakeDeps, CMakeToolchain
+
+class uFilterPickerProxyConan(ConanFile):
+   name = "uFilterPickerProxy"
+   #version = "0.0.1"
+   license = "MIT"
+   description = "Runs the Lomax-style filter picker in the UUSS Kubernetes environment."
+   url = "https://github.com/uofuseismo/uFilterPickerProxy"
+   #topics = ("uFilterPickerProxy")
+   settings = "os", "compiler", "build_type", "arch"
+   options = {"build_tests" : [True, False],
+              "build_training" : [True, False],
+              "with_conan" : [True, False]}
+   default_options = {"hwloc/*:shared": "True",
+                      "opentelemetry-cpp/*:with_otlp_http": "True",
+                      "opentelemetry-cpp/*:with_otlp_grpc": "True",
+                      "opentelemetry-cpp/*:with_abi_v2" : "True",
+                      "spdlog/*:header_only" : "True",
+                      "build_tests" : "True",
+                      "build_training" : "False",
+                      "with_conan" : "True",}
+   export_sources = "CMakeLists.txt", "LICENSE", "README.md", "cmake/*", "src/*", "testing/*"
+   generators = "CMakeDeps", "CMakeToolchain"
+
+   def requirements(self):
+       # dependencies
+       self.requires("grpc/1.78.1")
+       self.requires("opentelemetry-cpp/1.24.0")
+       self.requires("protobuf/6.33.5")
+       self.requires("boost/1.89.0")
+       self.requires("spdlog/1.17.0")
+       self.requires("sqlite3/3.53.0")
+       self.requires("onetbb/2022.3.0")
+
+   def build_requirements(self):
+       # test dependncies and build tools
+       self.test_requires("catch2/3.14.0")
+
+   def layout(self):
+       # defines the project layout
+       cmake_layout(self)
+ 
+   def build(self):
+       # invokes the build system
+       cmake = CMake(self)
+       cmake.configure()
+       cmake.build()
+       #if can_run(self):
+       #   # run tests particularly CTest 
+       #   cmake.test()
+
+   def test(self):
+       if can_run(self):
+          cmake.test()
+
+   #def generate(self):
+   #    tc = CMakeToolchain(self)
+   #    tc.generate()
+
+   def package(self):
+       # copies files from the build to package folder
+       cmake = CMake(self)
+       cmake.install()
+
+   def package_info(self):
+       self.cpp_info.libs = ["uFilterPickerProxy"]
+
